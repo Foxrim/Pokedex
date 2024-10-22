a98682d7b8f07a0946b39e4dc837910855b911c8
Foxrim <Flavien085@gmail.com>
2024-10-22 21:11:37 +0200

    [feat] React Basics 06

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,55 @@ import './App.css'
 import MyTitle from './components/MyTitle'
 import PokemonCard from './components/PokemonCard'
 
-function App() {
-  
-  const pokemonList = [
-    {
-      name: "Salamèche",
-      imgSrc:"./public/Salamèche.png",
-      alt: "Salamèche"
-    },
-    {
-      name: "Mew",
-      imgSrc:"./public/Mew.png",
-      alt: "Mew"
-    }
-];
+import { useState } from "react";
 
+export default function App() {
+  const [pokemonIndex, countPrev] = useState(0);
+
+  const prevIndex = () => {
+      countPrev(pokemonIndex - 1);
+  };
+
+  const nextIndex = () => {
+    countPrev(pokemonIndex + 1);
+  };
+  
   return (
     <>
       <div>
         <MyTitle />
-        <PokemonCard pokemon={ pokemonList[1] } />
+        <PokemonCard pokemon = { pokemonList[pokemonIndex] } />
+        {(pokemonIndex > 0) && <button type="button" onClick={prevIndex}>Précedent</button>}
+        {(pokemonIndex < pokemonList.length - 1) && <button type="button" onClick={nextIndex}>Suivant</button>}
       </div>        
     </>
   )
-}
+};
 
-export default App
-
+const pokemonList = [
+  {
+    name: "Salamèche",
+    imgSrc:"./public/Salamèche.png",
+    alt: "Salamèche"
+  },
+  {
+    name: "Mew",
+    imgSrc:"./public/Mew.png",
+    alt: "Mew"
+  },
+  {
+    name: "Bulbizar",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Carapuce",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "Pikachu",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "Mewtoo",
+  }
+];
