89e87c9fae473912cbdc6e639f2b3206cb616be5
Foxrim <Flavien085@gmail.com>
2024-10-22 11:22:02 +0200

    [feat] - Pokedex

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import MyTitle from './MyTitle'
+
+function PokemonCard() {
+
+  return (
+    <>
+      <section>
+        <MyTitle />
+        <img src="./public/Salamèche.png" alt="Salamèche" />  
+        <h2>Salamèche</h2>
+      </section>        
+    </>
+  )
+}
+
+export default PokemonCard
---
+++ src/MyTitle.tsx
@@ -0,0 +1,5 @@
+function MyTitle() {
+    return <h1>Pokemon !!!</h1>;
+  }
+  
+  export default MyTitle;
