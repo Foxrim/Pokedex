d8f687c19dab03590a86046dbfc59771810f2b54
Foxrim <Flavien085@gmail.com>
2024-10-23 16:10:01 +0200

    [fix] correction WOW trop bien NavBar j'avais tout compris depuis le début

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,48 @@
 import './App.css'
 import MyTitle from './components/MyTitle'
 import NavBar from './components/NavBar';
+import { useState } from "react";
+import PokemonCard from './components/PokemonCard';
 
 
 export default function App() {
- 
+
+const [pokemonIndex, countPrev] = useState(0);
+
+const pokemonList = [
+  {
+    name: "Salamèche",
+    imgSrc:"./public/Salamèche.png",
+    alt: "Salamèche"
+  },
+  {
+    name: "Mew",
+    imgSrc:"./public/Mew.png",
+    alt: "Mew"
+  },
+  {
+    name: "Bulbizar",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Carapuce",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "Pikachu",
+    imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
+    name: "Mewtoo",
+  }
+];
+
   return (
     <>
       <div>
         <MyTitle />
-        <NavBar />
+        <PokemonCard pokemon = { pokemonList[pokemonIndex] } />
+        <NavBar pokemonList={pokemonList} pokemonIndex={pokemonIndex} countPrev={countPrev} />
       </div>        
     </>
   )
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
-import PokemonCard from "./PokemonCard";
+interface Pokemon {
+  name: string;
+  imgSrc?: string;
+  alt?: string
+}
 
-export default function NavBar () {
+interface NavBarProps {
+  pokemonIndex: number;
+  countPrev: (index: number) => void;
+  pokemonList: Pokemon[];
+}
 
-
-const [pokemonIndex, countPrev] = useState(0);
+export default function NavBar ({pokemonList, pokemonIndex, countPrev}: NavBarProps) {
 
 const prevIndex = () => {
 countPrev(pokemonIndex - 1);
@@ -16,38 +22,8 @@ countPrev(pokemonIndex + 1);
 
     return (
         <>
-        <PokemonCard pokemon = { pokemonList[pokemonIndex] } />
         {pokemonIndex > 0 && <button type="button" onClick={prevIndex}>Précedent</button>}
         {pokemonIndex < pokemonList.length - 1 && <button type="button" onClick={nextIndex}>Suivant</button>}
         </>
     )
 };
-
-
-const pokemonList = [
-    {
-      name: "Salamèche",
-      imgSrc:"./public/Salamèche.png",
-      alt: "Salamèche"
-    },
-    {
-      name: "Mew",
-      imgSrc:"./public/Mew.png",
-      alt: "Mew"
-    },
-    {
-      name: "Bulbizar",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "Carapuce",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
-    },
-    {
-      name: "Pikachu",
-      imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-    },
-    {
-      name: "Mewtoo",
-    }
-  ];
